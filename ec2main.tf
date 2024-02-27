d114ffe1409a19ff861a39ff5eda61fa7c2751ea
Afsha10 <unityofbeing1@gmail.com>
2024-02-27 12:52:21 +0000

    Add inbound and outbound rules for security group using Terraform

--- ec2main.tf
+++ ec2main.tf
@@ -17,11 +17,37 @@ resource "aws_instance" "cyf_backend" {
 resource "aws_security_group" "cyf_backend_sg" {
   name        = "ec2_full_stack_project-security-group"  # Choose a unique name
   description = "Security group for ec2_full_stack_project-security-group backend"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # For SSH access, restrict this in production
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+
+  ingress {
+      from_port        = 5000
+      to_port          = 5000
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
   # Add more ingress rules for your application as needed
 }
+
